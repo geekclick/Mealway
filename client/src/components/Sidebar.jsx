090e57e62a086f107328f44e98fdf3228f4cb7d7
geekclick <anujghom27@gmail.com>
2024-03-03 23:05:39 +0530

    improve sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -7,9 +7,11 @@ import { IoWalletOutline } from "react-icons/io5";
 import { HiOutlineLocationMarker } from "react-icons/hi";
 import { CiDiscount1 } from "react-icons/ci";
 import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
 
 function Sidebar() {
   const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.authSlice.isLoggedIn);
   const sidebarOpen = useSelector((state) => state.sidebarSlice.sidebarOpen);
   const sidebarRef = useRef(null);
 
@@ -38,13 +40,19 @@ function Sidebar() {
       className="fixed z-20 min-h-screen w-[300px] p-[24px] bg-white flex flex-col space-y-10"
     >
       <div className=" space-y-8">
-        <div className="flex space-x-2">
-          <img src={profileImg} alt="" className="w-[48px] h-[48px]" />
-          <div className="flex flex-col -space-y-1">
-            <h2 className="font-medium italic">Anuj Ghom</h2>
-            <small>habhainakliemailhai@gmail.com</small>
+        {isLoggedIn ? (
+          <div className="flex space-x-2">
+            <img src={profileImg} alt="" className="w-[48px] h-[48px]" />
+            <div className="flex flex-col -space-y-1">
+              <h2 className="font-medium italic">Anuj Ghom</h2>
+              <small>habhainakliemailhai@gmail.com</small>
+            </div>
           </div>
-        </div>
+        ) : (
+          <Link to={"/signup"}>
+            <Button className="w-full">Register</Button>
+          </Link>
+        )}
         <div>
           <ul className="flex flex-col space-y-4">
             <li className=" font-medium flex italic">
