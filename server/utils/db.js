d85fd0c7c0c59536c3c060b26be17a9a7d1c10a0
geekclick <anujghom27@gmail.com>
2024-03-04 14:45:38 +0530

    mongodb error solved

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-<<<<<<< HEAD
-const  URL = "mongodb://127.0.0.1:27017/MealWay";
-=======
 const URL = process.env.MONGO_URI
->>>>>>> a9d46776db0ad957885f04c1b980be5dd4196b3b
 
 const connectionDB = async () => {
     try {
