c7f7878a52eeffbc33c7ddebf74f955ec75daf07
geekclick <anujghom27@gmail.com>
2024-03-04 15:03:14 +0530

    solve backend error

--- server/app.js
+++ server/app.js
@@ -5,12 +5,14 @@ const vandorRoute = require('./router/vendor-route');
 const cors = require('cors');
 
 const corsoptions = {
-  origin: 'http://localhost:5173/',
-  methods: "GET,POST,PUT,DELETE,OPTIONS,PATCH",
-  Credential: true,
+    origin: 'http://localhost:5173/',
+    methods: "GET,POST,PUT,DELETE,OPTIONS,PATCH",
+    Credential: true,
 }
 
 app.use(cors(corsoptions))
 app.use(express.json());
 app.use("/api", authRoute);
 app.use("/api", vandorRoute);
+
+module.exports = app;
