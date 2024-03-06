19bbbfa8981325df4d72c54e198ea5f92835a6e7
geekclick <anujghom27@gmail.com>
2024-03-06 22:19:37 +0530

    fix map UI errors

--- client/src/pages/Map.jsx
+++ client/src/pages/Map.jsx
@@ -5,12 +5,12 @@ import { Link } from "react-router-dom";
 import map from "@/assets/map.png";
 import BottomNav from "@/components/BottomNav";
 import { Button } from "@/components/ui/button";
-import MapComponent from "@/components/ui/MapComponent";
+import MapComponent from "@/components/MapComponent";
 
 function Map() {
   return (
     <section>
-      <div className="fixed  bg-white w-full flex justify-center items-center space-x-4 shadow-md py-5 px-4">
+      <div className="fixed z-50  bg-white w-full flex justify-center items-center space-x-4 shadow-md py-5 px-4">
         <Link to="/" className="flex justify-center items-center">
           <FaChevronLeft className="scale-150 text-muted-foreground" />
         </Link>
@@ -24,22 +24,18 @@ function Map() {
             className="border-none focus-visible:ring-0 focus-visible:ring-offset-0 bg-[#ECF1F6] w-full h-10"
           />
         </div>
-       
       </div>
-      <MapComponent/>
-      
+      <MapComponent />
+
       <div className={`bg-center w-full h-screen`}>
-        <Button variant="outline" className="absolute bottom-20 right-4">
+        <Button variant="outline" className="absolute z-50 bottom-20 right-4">
           Scan Nearby
         </Button>
       </div>
-      
+
       <BottomNav />
     </section>
   );
 }
 
 export default Map;
-
-
-
