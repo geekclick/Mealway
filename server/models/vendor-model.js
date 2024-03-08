0a0de9a87b913910622a7014e83f9e1645cbd362
pranavbhosale808 <pranavbhosale04@gmail.com>
2024-03-08 10:40:13 +0530

    Modifiy the Vendor model

--- server/models/vendor-model.js
+++ server/models/vendor-model.js
@@ -1,69 +1,94 @@
 const mongoose = require('mongoose');
-const schema = mongoose.Schema;
 
-const VendorSchema = new mongoose.Schema({
-    img: {
-        type: String,
-        required: false
+const menuItemSchema = new mongoose.Schema({
+   name: {
+      type: String,
+      required: true
     },
-    coverImg: {
-        type: String,
-        required: false
+    description: {
+      type: String,
+      required: true
     },
-    name: {
-        type: String,
-        required: true
+    category: {
+      type: String,
+      enum: ['Street Food', 'Fast Food', 'Snacks', 'Desserts', 'Beverages'],
+      required: true
     },
-    shopname: {
-        type: String,
-        required: true
+    price: {
+      type: Number,
+      required: true
+    },
+    vendor: {
+      type: mongoose.Schema.Types.ObjectId, // Reference to Vendor model
+      ref: 'Vendor', // Indicates that this field references the Vendor model
+      required: false
     },
     location: {
-        type: {
-            type: String,
-            enum: ['Point'],
-            required: false
-        },
-        coordinates: {
-            type: [Number],
-            required: false
-        }
+      type: String,
+      required: true
     },
-    address: {
-        type: String,
-        required: true
+    image: {
+      type: String, // URL of the image
+      required: false
     },
-    contact: {
-        type: String,
-        required: true
+    rating: {
+      type: Number,
+      default: 0
     },
-    ratings: {
-        type: Number,
-        default: 0,
-        required: false
-    },
-    reviews: [
-        {
-            type: String,
-            required: false
-
-        }
-    ],
-    openCloseHours: {
-        type: Map,
-        of: String,
-        required: false
-
-    },
-    menu: [{
-        dishName: String,
-        price: Number,
-        description: String,
-    }]
 });
 
-VendorSchema.index({ location: "2dsphere" });
-
-const Vendor = new mongoose.model("Vendor", VendorSchema);
+const vendorSchema = new mongoose.Schema({
+   img: {
+      type: String,
+      required: false
+  },
+  coverImg: {
+      type: String,
+      required: false
+  },
+ name: {
+    type: String,
+    required: true,
+    trim: true
+ },
+ shopname: {
+    type: String,
+    required: true,
+    trim: true
+ },
+ location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      index: '2dsphere'
+    }
+ },
+ description: {
+    type: String,
+    trim: true
+ },
+ menu: {
+    type: [menuItemSchema],
+    required: true
+ },
+ contact: {
+    type:Number,
+    required: true
+ },
+ openingHours: {
+    type: Map,
+    of: String,
+    required: true
+ }
+}
+);
+vendorSchema.index({ 'menu.name': 'text' });
+// Create a 2dsphere index for the location field
+vendorSchema.index({ location: '2dsphere' });
 
 module.exports = Vendor;
