e389600bfe5dc368d34c5777075f1bc3ce83a49f
geekclick <anujghom27@gmail.com>
2024-03-09 08:05:33 +0530

    create random on home page

--- client/src/components/homepage/Offers.jsx
+++ client/src/components/homepage/Offers.jsx
@@ -5,10 +5,20 @@ import {
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 
+import v1 from "@/assets/carousel/v1.mp4";
+import v2 from "@/assets/carousel/v2.mp4";
+import v3 from "@/assets/carousel/v3.mp4";
+import v4 from "@/assets/carousel/v4.mp4";
+import v5 from "@/assets/carousel/v5.mp4";
+import v6 from "@/assets/carousel/v6.mp4";
+import v7 from "@/assets/carousel/v7.mp4";
+
+const videos = [v1, v2, v3, v4, v5, v6, v7];
+
 function Offers() {
   return (
     <section className="flex justify-center items-start flex-col w-full space-y-2">
-      <h2 className="ml-2 italic">Special offer</h2>
+      <h2 className="ml-2 italic">Explore the local delicacies</h2>
       <Carousel
         opts={{
           align: "start",
@@ -16,40 +26,27 @@ function Offers() {
         }}
         plugins={[
           Autoplay({
-            delay: 2000,
+            delay: 8000,
           }),
         ]}
         className="flex justify-center items-center w-full"
       >
         <CarouselContent>
-          <CarouselItem>
-            <img
-              src={"https://source.unsplash.com/327x148/?cake$1"}
-              alt=""
-              className="rounded-lg m-auto"
-            />
-          </CarouselItem>
-          <CarouselItem>
-            <img
-              src={"https://source.unsplash.com/327x148/?burger$2"}
-              alt=""
-              className="rounded-lg m-auto"
-            />
-          </CarouselItem>
-          <CarouselItem>
-            <img
-              src={"https://source.unsplash.com/327x148/?piza$3"}
-              alt=""
-              className="rounded-lg m-auto"
-            />
-          </CarouselItem>
-          <CarouselItem>
-            <img
-              src={"https://source.unsplash.com/327x148/?noodles$4"}
-              alt=""
-              className="rounded-lg m-auto"
-            />
-          </CarouselItem>
+          {videos.map((item, i) => {
+            return (
+              <CarouselItem key={i}>
+                <video
+                  src={item}
+                  width="327"
+                  height="148"
+                  autoPlay
+                  muted
+                  loop
+                  className="rounded-xl"
+                ></video>
+              </CarouselItem>
+            );
+          })}
         </CarouselContent>
       </Carousel>
     </section>
